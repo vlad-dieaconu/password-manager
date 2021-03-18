8fc2cea4448e2aaa4402d97724c0c721683ce686
vlad-dieaconu <vladdieaconu@yahoo.com>
2021-03-18 14:58:49 +0200

    Added user class

---
+++ Server/src/main/java/com/ic/passwordmanager/model/User.java
@@ -0,0 +1,35 @@
+package com.ic.passwordmanager.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+
+    private int id;
+    private String email;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
