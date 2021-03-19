af4deabbd525de6320504d5c4ab1000a34dd0589
vlad-dieaconu <vladdieaconu@yahoo.com>
2021-03-19 13:29:52 +0200

    Added UserController class for POSTMAN test

---
+++ Server/src/main/java/com/ic/passwordmanager/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.ic.passwordmanager.controller;
+
+import com.ic.passwordmanager.model.User;
+import com.ic.passwordmanager.service.FirebaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ExecutionException;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    FirebaseService firebaseService;
+
+    @GetMapping("/getUserCredentials")
+    public User getExample(@RequestHeader() String email) throws ExecutionException, InterruptedException {
+        return firebaseService.getUserDetails(email);
+    }
+    @PostMapping("/createUser")
+    public String postExample(@RequestBody User user) throws ExecutionException, InterruptedException {
+        return firebaseService.saveUserDetails(user);
+    }
+    @PutMapping("/updateUser")
+    public String putExample(@RequestBody User user){
+        return "Updated user " + user.getEmail();
+    }
+
+
+}
+
