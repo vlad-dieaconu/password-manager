7ca47a4eb0b236fde829276a277221df16bd8567
vlad-dieaconu <vladdieaconu@yahoo.com>
2021-03-19 15:40:09 +0200

    Modified user class for database tests

--- Server/src/main/java/com/ic/passwordmanager/model/User.java
+++ Server/src/main/java/com/ic/passwordmanager/model/User.java
@@ -2,22 +2,34 @@ package com.ic.passwordmanager.model;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+
 @Component
 public class User {
 
 
     private String email;
     private String password;
+    private ArrayList<Account> accounts;
 
-    public User(String email, String password) {
+
+
+    public User(String email, String password, ArrayList<Account> accounts) {
         this.email = email;
         this.password = password;
+        this.accounts = accounts;
     }
 
     public User() {
     }
 
+    public ArrayList<Account> getAccounts() {
+        return accounts;
+    }
 
+    public void setAccounts(ArrayList<Account> accounts) {
+        this.accounts = accounts;
+    }
 
     public String getEmail() {
         return email;
