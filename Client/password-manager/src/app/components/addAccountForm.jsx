ad741916596025da00f170468884ce84b6cef9a1
vlad-dieaconu <vladdieaconu@yahoo.com>
2021-04-14 10:49:33 +0300

    added authorization header in axios post request

--- Client/password-manager/src/app/components/addAccountForm.jsx
+++ Client/password-manager/src/app/components/addAccountForm.jsx
@@ -1,55 +1,65 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import axios from "axios";
 
 class AddAccountForm extends Component {
-    
-    addAccount = async (platforma, password) => {
 
+    
+
+
+
+    addAccount = async (platforma, password) => {
         const userID = JSON.parse(localStorage.getItem('user'));
-        console.log(userID.id);
-        return axios.post("/users/accounts/"+userID.id+"/addAccount",{
+        const accessToken = userID.accessToken
+
+        const authAxios = axios.create({
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+
+
+        return authAxios.post("/users/accounts/" + userID.id + "/addAccount", {
+
             platforma,
             password
         })
-        .then(response => {
-            
-        })
-        .catch(err => {
-            console.log(err);
-            throw err;
-        });
+            .then((response) => {
+                console.log(response);
+            }, (error) => {
+                console.log(error);
+            });
     }
 
     backToProfile = () => {
         this.props.history.push('/profile');
     }
 
-    render(){
+    render() {
         return (
-            <div style={{marginTop:"20px"}}>
+            <div style={{ marginTop: "20px" }}>
                 <h2>Add a new platform</h2>
                 <ul>
-                <input type="text"
-                    name="platforma"
-                    id="platforma"
-                    placeholder="Platform"
-                />
+                    <input type="text"
+                        name="platforma"
+                        id="platforma"
+                        placeholder="Platform"
+                    />
                 </ul>
 
                 <ul>
-                <input type="password"
-                    name="password"
-                    id="password"
-                    placeholder="Password"
-                />
+                    <input type="password"
+                        name="password"
+                        id="password"
+                        placeholder="Password"
+                    />
                 </ul>
                 <button onClick={this.addAccount}>ADD</button>
                 <button onClick={this.backToProfile}>Back to profile</button>
             </div>
         );
-        
+
     }
-        
+
 }
 
 export default AddAccountForm;
