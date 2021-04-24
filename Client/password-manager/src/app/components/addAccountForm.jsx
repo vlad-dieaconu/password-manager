0de731dbf5f7039d845277754db2ff25cb221398
viviana-gherasim <viviana-gherasim@users.noreply.github.com>
2021-04-24 22:00:34 +0300

    styled the component for adding a new account

--- Client/password-manager/src/app/components/addAccountForm.jsx
+++ Client/password-manager/src/app/components/addAccountForm.jsx
@@ -15,7 +15,7 @@ class AddAccountForm extends Component {
     changeHandler = (event) => {
         const { name, value } = event.target;
         this.setState({ [name]: value }, () => {
-            
+
         })
     }
 
@@ -41,42 +41,50 @@ class AddAccountForm extends Component {
         );
     }
 
-backToProfile = () => {
-    this.props.history.push('/profile');
-}
+    backToProfile = () => {
+        this.props.history.push('/profile');
+    }
 
-render() {
-    return (
-        <Form onSubmit={this.addNewAccount}>
-        <div style={{ marginTop: "100px", marginLeft: "500px"}}>
-            <h2>Add a new platform</h2>
-            <ul>
-                <input type="text"
-                    name="platforma"
-                    id="platforma"
-                    placeholder="Platforma"
-                    value={this.state.platforma}
-                    onChange={this.changeHandler}
+    render() {
+        return <body>
+            <div class="container">
+                <div class="forms-container">
+                    <form onSubmit={this.addNewAccount}>
+                        <div style={{ marginTop: "100px", marginLeft: "50px" }}>
+                        <h2 class="title">Add a new platform</h2>
+                        <br></br>
+                        <div class="input-field">
+                            <i class="email-icon"></i>
+                            <input type="text"
+                                name="platforma"
+                                id="platforma"
+                                placeholder="Platform"
+                                value={this.state.platforma}
+                                onChange={this.changeHandler}
+                            />
+                        </div>
+                        
+                        <div class="input-field">
+                            <i class="password-icon"></i>
+                            <input type="password"
+                                name="password"
+                                id="password"
+                                placeholder="Password"
+                                value={this.state.password}
+                                onChange={this.changeHandler}
+                            />
+                        </div>
 
-                />
-            </ul>
+                        <button style={{marginRight: "50px"}} type="submit" class="btn-solid" onSubmit={this.addNewAccount}>ADD</button>
+                        <button type="submit" class="btn-solid" onClick={this.backToProfile}>Back to profile</button>
+                        </div>
+                    </form>
 
-            <ul>
-                <input type="password"
-                    name="password"
-                    id="password"
-                    placeholder="Password"
-                    value={this.state.password}
-                    onChange={this.changeHandler}
-                />
-            </ul>
-             
-            <button onSubmit={this.addNewAccount}>ADD</button>
-            <button onClick={this.backToProfile}>Back to profile</button>
-        </div></Form>
-    );
+                </div>
+            </div>
+        </body>
 
-}
+    }
 
 }
 
