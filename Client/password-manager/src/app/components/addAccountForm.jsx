34226dcb2353b6b2037e97b106d48dad01cc7226
viviana-gherasim <viviana-gherasim@users.noreply.github.com>
2021-05-08 00:36:54 +0300

    account added successfully alert

--- Client/password-manager/src/app/components/addAccountForm.jsx
+++ Client/password-manager/src/app/components/addAccountForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import BackendService from "../services/BackendService";
+import { Alert } from "reactstrap";
 
 class AddAccountForm extends Component {
 
@@ -45,37 +46,65 @@ class AddAccountForm extends Component {
     }
 
     render() {
+        let alert = "";
+
+        if (this.state.message) {
+            if (this.state.successful) {
+                alert = (
+                    <Alert variant="success">
+                        {this.state.message}
+                    </Alert>
+                );
+
+
+            } else {
+                alert = (
+                    <Alert variant="danger">
+                        {this.state.message}
+                    </Alert>
+                );
+            }
+        }
         return <body>
             <div class="container">
                 <div class="forms-container">
                     <form onSubmit={this.addNewAccount}>
                         <div style={{ marginTop: "100px", marginLeft: "50px" }}>
-                        <h2 class="title">Add a new platform</h2>
-                        <br></br>
-                        <div class="input-field">
-                            <i class="email-icon"></i>
-                            <input type="text"
-                                name="platforma"
-                                id="platforma"
-                                placeholder="Platform"
-                                value={this.state.platforma}
-                                onChange={this.changeHandler}
-                            />
-                        </div>
-                        <br></br>
-                        <div class="input-field">
-                            <i class="password-icon"></i>
-                            <input type="password"
-                                name="password"
-                                id="password"
-                                placeholder="Password"
-                                value={this.state.password}
-                                onChange={this.changeHandler}
-                            />
-                        </div>
+                            <h2 class="title">Add a new platform</h2>
+                            <br></br>
+                            <div class="input-field">
+                                <i class="email-icon"></i>
+                                <input type="text"
+                                    name="platforma"
+                                    id="platforma"
+                                    placeholder="Platform"
+                                    value={this.state.platforma}
+                                    onChange={this.changeHandler}
+                                />
+                            </div>
+                            <br></br>
+                            <div class="input-field">
+                                <i class="password-icon"></i>
+                                <input type="password"
+                                    name="password"
+                                    id="password"
+                                    placeholder="Password"
+                                    value={this.state.password}
+                                    onChange={this.changeHandler}
+                                />
+                            </div>
 
-                        <button style={{marginRight: "50px"}} type="submit" class="btn-solid" onSubmit={this.addNewAccount}>ADD</button>
-                        <button type="submit" class="btn-solid" onClick={this.backToProfile}>Back to profile</button>
+                            <button style={{ marginRight: "50px" }} type="submit" class="btn-solid" onSubmit={this.addNewAccount}>ADD</button>
+
+                            <button type="submit" class="btn-solid" onClick={this.backToProfile}>Back to profile</button>
+                            {
+                                this.state.successful && (
+                                    <Alert variant="success">
+                                        Account added successfully!
+                                    </Alert>
+                                )
+                            }
+                            {alert}
                         </div>
                     </form>
 
